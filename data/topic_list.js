9aa601ba44e83102e028cc1d80b328ea1082216b
usoni1 <usoni1@asu.edu>
2017-11-19 21:22:19 -0700

    added topic list

---
+++ data/topic_list.js
@@ -0,0 +1,20 @@
+var topic_list = [
+    "2D_Arrays",
+    "Arithmetics",
+    "Arrays",
+    "Classes",
+    "Constructor",
+    "Control",
+    "DataType",
+    "Decisions",
+    "Expression",
+    "InputOutput",
+    "Java",
+    "Loops",
+    "Method",
+    "Objects",
+    "Operator",
+    "Primitive Data Type",
+    "Strings",
+    "Variables"
+];
